f49b454eb1cb277c5e5bd764fabb51b53f963b14
SSMurthy619 <shreepadmurthy2@gmail.com>
2026-01-28 23:02:49 +0530

    Structured directory for sequential addition of new programs.

---
+++ Foundational_Cpp/accessing_updating_variable.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num;
+    num = 10;
+    cout << num << endl;
+
+    num = 12; // Updating the variable "num" which was already initialized with a value = 10
+    cout << num << endl;
+
+    return 0;
+}

507456bdbfe0d1cae7025aefa9a86b49d2a46def
SSMurthy619 <shreepadmurthy2@gmail.com>
2026-01-29 23:04:31 +0530

    Simple working of basic Arithmetic Operators.

---
+++ Foundational_Cpp/arithmetic_operators.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    float num1, num2;
+
+    cout << "Enter the first number > " << endl;
+    cin >> num1;
+
+    cout << "Enter the second number > " << endl;
+    cin >> num2;    
+
+    // Addition
+    cout << "Addition of the two numbers is: " << num1 + num2 << endl;
+    
+    // Subtraction
+    cout << "Subtraction of the two numbers is: " << num1 - num2 << endl;
+
+    // Multiplication
+    cout << "Multiplication of the two numbers is: " << num1 * num2 << endl;
+
+    // Division
+    cout << "Division of the two numbers is: " << num1 / num2 << endl;
+
+    return 0;
+}
